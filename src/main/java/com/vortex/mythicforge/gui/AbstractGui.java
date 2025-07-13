253ccaad791732a9fe857e2dc802928f4f24bb0b
Kazumiii-arch <weskyboundrealms@gmail.com>
2025-07-13 13:49:36 +0800

    Update AbstractGui.java

--- src/main/java/com/vortex/mythicforge/gui/AbstractGui.java
+++ src/main/java/com/vortex/mythicforge/gui/AbstractGui.java
@@ -11,29 +11,31 @@ import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
 
 public abstract class AbstractGui implements Listener {
-
-    protected final MythicForge plugin = MythicForge.getInstance();
+    protected final MythicForge plugin;
     protected final Player player;
     protected Inventory inventory;
 
     public AbstractGui(Player player) {
+        this.plugin = MythicForge.getInstance();
         this.player = player;
     }
 
     protected abstract Inventory createInventory();
-
     protected abstract void handleClick(InventoryClickEvent event);
 
     public void open() {
         this.inventory = createInventory();
         Bukkit.getPluginManager().registerEvents(this, plugin);
-        player.openInventory(this.inventory);
+        if (player != null && player.isOnline()) {
+            player.openInventory(this.inventory);
+        }
     }
 
     @EventHandler
     public final void onInventoryClick(InventoryClickEvent event) {
         if (!event.getInventory().equals(inventory)) return;
-        if (event.getClickedInventory() != inventory && event.getClickedInventory() != null) return;
+        if (!(event.getWhoClicked() instanceof Player)) return;
+        if (event.getClickedInventory() != inventory) return;
         handleClick(event);
     }
 
@@ -43,8 +45,8 @@ public abstract class AbstractGui implements Listener {
             HandlerList.unregisterAll(this);
         }
     }
-    
+
     public Inventory getInventory() {
         return inventory;
     }
-            }
+}
